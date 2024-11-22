1f9a3e4efc27303b56775b6210d2530a8dcbfc5e
Jozphi <jozef.phillips65@gmail.com>
2024-11-23 07:16:57 +1100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+music.play(music.stringPlayable("E D G F B A C5 B ", 120), music.PlaybackMode.UntilDone)
+basic.showString("Hello Sophie!")
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
